1718b12c986c7501f92ef547317ae462e8c9e12f
unknown <yeghiazaryan99@gmail.com>
2019-02-28 22:53:34 +0400

    Some test with SendGrid

--- src/main/java/am/aca/quiz/software/service/HerokuMail.java
+++ src/main/java/am/aca/quiz/software/service/HerokuMail.java
@@ -11,17 +11,17 @@ public class HerokuMail {
 
     //TODO ASHXATACNEL VAHE JAN
     public void sendEmail(String fromEmail, String toEmail, String subject, String text) throws IOException {
-        Email from = new Email(fromEmail);
-        Email to = new Email(toEmail);
-        Content content = new Content("text/plain", text);
-        Mail mail = new Mail(from, subject, to, content);
-
-        SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-        Request request = new Request();
         try {
-            request.setMethod(Method.POST);
-            request.setEndpoint("mail/send");
-            request.setBody(mail.build());
+            SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+            Request request = new Request();
+            request.setMethod( Method.POST);
+            request.setEndpoint( "mail/send");
+            request.setBody("{\"personalizations\":[{\"to\":[{\"email\":\""
+                +toEmail+"\"}],\"subject\":\""
+                +subject+"\"}],\"from\":{\"email\":\""
+                +fromEmail+"\"},\"content\":[{\"type\":\"text/plain\",\"value\": \""
+                +text+"\"}]}");
+
             Response response = sg.api(request);
             System.out.println(response.getStatusCode());
             System.out.println(response.getBody());
